96c72739fabb57ba9c484324c982ea276c6e8b5c
Greedysky <greedysky@163.com>
2025-11-07 15:21:14 +0800

    feat: Support for qmmp 0.x Qt4

--- decoderoptimfrogfactory.cpp
+++ decoderoptimfrogfactory.cpp
@@ -116,3 +116,8 @@ QString DecoderOptimFROGFactory::translation() const
 {
     return QString();
 }
+
+#if QT_VERSION < QT_VERSION_CHECK(5,0,0)
+#include <QtPlugin>
+Q_EXPORT_PLUGIN2(optimfrog, DecoderOptimFROGFactory)
+#endif
